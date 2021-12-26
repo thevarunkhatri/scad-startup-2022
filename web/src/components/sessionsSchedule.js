b7c1a678fbcff3f8e400e390bb8c0bff14ce08b9
Varun Khatri <varunkhatri@Varuns-MacBook-Pro.local>
2021-12-26 13:21:13 -0500

    schedule sessions progress

--- web/src/components/sessionsSchedule.js
+++ web/src/components/sessionsSchedule.js
@@ -1,12 +1,56 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby"
 
+const SessionsCard = props => {
+
+  const imageStyle = {
+    background: 'url(' + props.imageUrl + ')',
+    backgroundSize: 'cover'
+  };
+
+  return (
+      <div className="sessionsCard">
+          <div className="heroImage" style={imageStyle}></div>
+          <div className="cardContent">
+            <h4>{props.name}</h4>
+            <div className="timeLocation">
+              <span>5PM</span>
+              <span>60m</span>
+              <a href=""><span>The Shed</span></a>
+            </div>
+            <div className="difficulty">
+              <span>Difficulty: </span>
+              <div className="difficultyCircles">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <span className="difficultyText">Beginner</span>
+            </div>
+            <p className="description">Placeholder content for the description of the session! Integer vel enim viverra nulla arcu. Velit enim cras turpis aliquam ullamcorper. Felis, convallis mattis elit commodo.</p>
+            <div className="cardButtons">
+              <button>Register</button>
+              <button>Zoom</button>
+            </div>
+          </div>
+      </div>
+  );
+};
+
 const SessionsSchedule = () => {
     const sessionsData = useStaticQuery(graphql`
         query sessionsQuery {
             allSanitySessions {
                 edges {
                   node {
+                    heroImage {
+                      _key
+                      asset {
+                        url
+                      }
+                    }
                     demoFiles {
                       _key
                       _type
@@ -52,6 +96,20 @@ const SessionsSchedule = () => {
     return (
         <div>
             <h1>Sessions</h1>
+            <div className="sessionsGrid">
+              {
+                sessionsArray.map(edge => 
+                    {
+                      console.log(edge)
+                      return(
+                        <SessionsCard name={edge.node.name} imageUrl={edge.node.heroImage.asset.url}/>
+                      )       
+                    }
+                )
+              }
+              <h3>Hello</h3>
+              <h3>Hello</h3>
+            </div>
         </div>
     );
 };
