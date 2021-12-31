9bd5edee71fb891f8b9c187c68f521267411ceda
MattVAhrens <57081517+MattVAhrens@users.noreply.github.com>
2021-12-31 12:31:00 -0500

    Initial footer design

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -7,7 +7,35 @@ const Footer = () => {
 
     return(
         <footer>
+            <div className="footerContainer">
 
+                <ul className="footerLeft">
+                     <Link to=""><li>
+                       <img src={"https://placeholder.pics/svg/60/75F3FF"}></img>
+                    </li></Link>
+                    <Link to=""><li>
+                       <img src={"https://placeholder.pics/svg/60/75F3FF"}></img>
+                    </li></Link>
+                    <Link to=""><li>
+                       {/* <img src={"https://placeholder.pics/svg/60/75F3FF"}></img> */}
+                    </li></Link>
+                </ul>
+
+                <Link to="/">
+                    <h3>StartUp</h3>
+                </Link>
+
+                <ul className="footerRight">
+                    <Link to=""><li>
+                       <img src={"https://placeholder.pics/svg/60/75F3FF"}></img>
+                    </li></Link>
+                    <Link to=""><li>
+                        <img src={"https://placeholder.pics/svg/60/75F3FF"}></img>
+                    </li></Link>
+                    <button class="footerButton">Sign up &nbsp; {String.fromCharCode(10230)}</button>
+                </ul>
+                
+            </div>
         </footer>
     );
 };
