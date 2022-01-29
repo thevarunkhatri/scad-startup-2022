c7e15aad3fe0a6172023c96108f99ade0ab478b2
MattVAhrens <57081517+MattVAhrens@users.noreply.github.com>
2022-01-29 10:41:59 -0500

    more judges!

--- web/src/pages/people.js
+++ web/src/pages/people.js
@@ -154,7 +154,7 @@ const PeoplePage = props => {
             img={varunProfile}
             fname="Varun"
             lname="Khatri"
-            role="Web Developer"
+            role="Lead Web Developer"
             major="User Experience"
             linkedin="https://www.linkedin.com/in/varunmk/"
             portfolio="https://www.thevarunkhatri.com/"
